ab791f1d12e58ca7aee5e6dee2dad3975bb6ece5
xieyong <1239665882@qq.com>
2025-01-07 13:33:10 +0800

    111

--- src/index.tsx
+++ src/index.tsx
@@ -24,19 +24,19 @@ app.frame("/", (c) => {
   });
 });
 
-// Frame to display user's response.
-app.frame("/submit", (c) => {
-  const { buttonValue } = c;
-  return c.res({
-    action: "/",
-    image: <div style={{ color: "black", fontSize: 60 }}>{buttonValue}</div>,
-    intents: [
-      <Button value="apple">Apple</Button>,
-      <Button value="banana">Banana</Button>,
-      <Button value="back">Back</Button>,
-    ],
-  });
-});
+// // Frame to display user's response.
+// app.frame("/submit", (c) => {
+//   const { buttonValue } = c;
+//   return c.res({
+//     action: "/",
+//     image: <div style={{ color: "black", fontSize: 60 }}>{buttonValue}</div>,
+//     intents: [
+//       <Button value="apple">Apple</Button>,
+//       <Button value="banana">Banana</Button>,
+//       <Button value="back">Back</Button>,
+//     ],
+//   });
+// });
 
 const port = 9528;
 console.log(`Server is running on port ${port}`);
